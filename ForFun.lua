e0dbf91bfe42c61b61fb3f0afc2674b5232cc793
joelhaapaniemi <joel.haapaniemi@outlook.com>
2025-05-25 20:15:15 +0300

    Added some functionality for retrieving player information

--- ForFun.lua
+++ ForFun.lua
@@ -1,2 +1,17 @@
-text = "Hello world!"
-message(text)
+-- Description: A simple Lua script for World of Warcraft that retrieves and displays player information.
+
+function getPlayerInformation()
+   playerName = UnitName("player")
+   playerLevel = UnitLevel("player")
+   playerClass = UnitClass("player")
+   playerRace = UnitRace("player")
+   playerFaction = UnitFactionGroup("player")
+   playerRealm = GetRealmName()
+   
+   
+   return "Greetings, " .. playerName .. " (Level " .. playerLevel .. ") " .. 
+   playerRace .. " " .. playerClass .. " of the " .. playerFaction .. " on the realm " .. 
+   playerRealm .. "."
+end
+
+message(getPlayerInformation())
